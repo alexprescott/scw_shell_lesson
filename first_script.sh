09f908e141d4991a6f0ec5d1b3374d13acc98c01
Alexander Prescott <alexprescott@email.arizona.edu>
2017-02-25 12:07:43 -0700

    Add comment to the script

--- first_script.sh
+++ first_script.sh
@@ -1,1 +1,8 @@
+# This is a comment! My first script in bash 
+# $1 is input, $2 is output to save to
+# Performs: 
+# ==> take first 5 lines from $1
+# ==> sort alphabetically
+# ==> count the number of characters
+# ==> save the count to $2
 head -5 $1 | sort | wc -c > $2
